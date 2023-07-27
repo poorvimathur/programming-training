990892837e500933b8b8e1695742031c87a29e1c
poorvimathur <68208551+poorvimathur@users.noreply.github.com>
2023-07-27 02:00:04 -0400

    Add files via upload

---
+++ Battleship.java
@@ -0,0 +1,10 @@
+public class Battleship {
+    public static void main(String[] args) {
+        int gridSize = 10;
+        int numShips = 5;
+
+        char[][] player1 = new char[gridSize][gridSize];
+        char[][] player2 = new char[gridSize][gridSize];
+    }
+
+}
